da19e081c88f3a3713d14a3215dd3c1893b8e723
Mathieu Sauvage <mathieus@server.fake>
2013-06-19 01:23:01 +0900

    batch numpy version beta

--- lightningboltProcessor.py
+++ lightningboltProcessor.py
@@ -51,13 +51,15 @@ class lightningBoltProcessor:
 		# column 2 is Offset
 		self.APVInputs1 = None
 		# this is a vector with all the multiplicator to apply to APVInputs
-		self.APVInputsMultiplier1 = np.zeros((1,lightningBoltProcessor.kAPV_MaxAttributes),np.float32)
+		self.APVInputsMultiplier1 = np.zeros((lightningBoltProcessor.kAPV_MaxAttributes),np.float32)
 
 		# has no variation along the path
 		# O numchildens (nombre d'enfant genere obligatoirement)
 		# 1 randNumChildrens (nombre d'enfant potentiellement genere aleatoirement en plus)
 
 		self.VInputsGeneral = np.zeros((lightningBoltProcessor.kV_MaxAttributes),np.float32)
+
+		#for testing
 		self.VInputsGeneral[0] = 2
 		self.VInputsGeneral[1] = 2
 
@@ -73,13 +75,30 @@ class lightningBoltProcessor:
 		return self.APVInputs1[:,num]
 
 	def setAPVMult1(self, num, value):
-		self.APVInputsMultiplier1[0,num] = value
+		self.APVInputsMultiplier1[num] = value
 
 	def setVGeneral(self, num, value):
 		self.VInputsGeneral[num] = value
+	
+	def initializeProcessor( self ):
+		self.startSeedPoints = []
+		self.numStartSeedBranch = 0
+		self.startSeedBranchesAttributes = []
 
-	def generate( self, branchInputs, doGenerateChilds ):
-		seedPath, APV, APVMult = branchInputs
+	def addSeedPointList( self, pointList):
+		self.startSeedPoints.extend( pointList )
+		#self.startSeedPoints.append([0,0,0]) # add an extra blank point
+		self.numStartSeedBranch =  self.numStartSeedBranch + 1
+		self.startSeedBranchesAttributes.append(self.APVInputsMultiplier1.reshape(1,-1))
+
+		# for testing
+		self.startSeedPoints.extend( [[ point[0],point[1]+2,point[2] ] for point in pointList] )
+		#self.startSeedPoints.append([0,0,0]) # add an extra blank point
+		self.numStartSeedBranch =  self.numStartSeedBranch + 1
+		self.startSeedBranchesAttributes.append(self.APVInputsMultiplier1.reshape(1,-1)*0.5)
+	'''
+	def generate( self, branchInputs, APV, doGenerateChilds ):
+		seedPath, APVMult = branchInputs
 		sys.stderr.write('APVMult '+str(APVMult)+'\n')
 
 		APArray = APV * APVMult # the attribute path Array contain every attribute value at every point
@@ -113,24 +132,197 @@ class lightningBoltProcessor:
 
 
 		return (seedPath, APArray) , []
+	'''
 
-	def process(self, pointList ):
+	def generate( self, batch, APV, isLooping, doGenerateChilds ):
+		
+		batchSize, seedPath, APVMults = batch
+		sys.stderr.write('APVMults '+str(APVMults)+'\n')
+		APVs = np.tile(APV,(batchSize,1,1)) 
+		sys.stderr.write('APVs '+str(APVs)+'\n')
+		APArray = APVs * APVMults # the attribute path Array contain every attribute value at every point
+		sys.stderr.write('APArray '+str(APArray)+'\n')
 
-		outBranchList = []
+		offsetArray = (np.array( arndSphereVect(batchSize*self.maxPoints) ,np.float32)).reshape(batchSize,self.maxPoints,3)
+		#import sys
+		#sys.stderr.write('offetArray (Points)'+str(offsetArray)+'\n')
+		#sys.stderr.write('offset '+str(APArray[:,lightningBoltProcessor.kAPV_Offset].reshape(self.maxPoints,-1))+'\n')
+		sys.stderr.write('seedPath '+str(seedPath)+'\n')
+		seedPathPointsView = seedPath.reshape(batchSize,self.maxPoints,3) #[:,:-1] #just to remove
+		sys.stderr.write('seedPathPointsView '+str(seedPathPointsView)+'\n')
+		sys.stderr.write('offsetArray '+str(offsetArray)+'\n')
+		#temp = APArray[:,:,:,lightningBoltProcessor.kAPV_Offset].reshape(1,1,batchSize,self.maxPoints,-1)
+		#sys.stderr.write('temp '+str(temp)+'\n')
+		#sys.stderr.write('offsetArray*temp '+str((offsetArray*temp)[0,0])+'\n')
+
+		seedPathPointsView += (offsetArray*APArray[:,:,:,lightningBoltProcessor.kAPV_Offset].reshape(1,1,batchSize,self.maxPoints,-1))[0,0]
+		sys.stderr.write('seedPathPointsView '+str(seedPathPointsView)+'\n')
+		sys.stderr.write('seedPath '+str(seedPath)+'\n')
+
+		# let's create the frame on all the paths
+		frameTemplate = np.array( [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,1]], np.float32 )
+		frames = np.tile(frameTemplate,(batchSize,self.maxPoints,1,1))
+
+		# -------------- calculate the vector front at every point
+		frameFronts = frames[:,:,0,:-1]
+		#sys.stderr.write('frameFronts '+str(frameFronts)+'\n')
+		#seedPathView = seedPath.reshape(batchSize,self.maxPoints+1,-1)
+
+		#sys.stderr.write('seedPathView '+str(seedPathView)+'\n')
+		unitDir = (np.roll( seedPathPointsView, -1, axis=1 )[:,:-1])
+		sys.stderr.write('unitDir '+str(unitDir)+'\n')
+		unitDir -= seedPathPointsView[:,:-1]
+		sys.stderr.write('unitDir '+str(unitDir)+'\n')
+		normFrontSqr = np.sqrt(((unitDir**2).sum(-1)).reshape(batchSize,self.maxPoints-1,1))
+		sys.stderr.write('normFrontSqr '+str(normFrontSqr)+'\n')
+		unitDir /= normFrontSqr
+		sys.stderr.write('unitDir '+str(unitDir)+'\n')
+
+		sys.stderr.write('frameFronts '+str(frameFronts)+'\n')
+		frameFronts[:,:-1,:][...] = unitDir
+		frameFronts[:,1:,:] += unitDir
+		sys.stderr.write('frameFronts '+str(frameFronts)+'\n')
+
+		if isLooping :
+			frameFronts[:,0,:] += frameFronts[:,-1,:] # we add the last direction of the branch to the first direction
+			# we normalize all the direction except the last one ( -> :-1 )
+			normFrontSqr = np.sqrt(((frameFronts[:,:-1,:]**2).sum(-1)).reshape(batchSize,self.maxPoints-1,1))
+			frameFronts[:,:-1,:] /= normFrontSqr
+		else : # we normalize all the direction except the first one and the last one ( -> 1:-1 )
+			normFrontSqr = np.sqrt(((frameFronts[:,1:-1,:]**2).sum(-1)).reshape(batchSize,self.maxPoints-2,1))
+			frameFronts[:,1:-1,:] /= normFrontSqr
+
+		sys.stderr.write('frameFronts '+str(frameFronts)+'\n')
+
+	#------ the algorithm to setup the frame for each point need the first frame to be completely initialized for every branch
+		firstFrameViewFront = frames[:,0,0,:-1]
+		firstFrameViewUp = frames[:,0,1,:-1]
+		firstFrameViewSide = frames[:,0,2,:-1]
+		#sys.stderr.write('firstFrameViewUp '+str(firstFrameViewUp)+'\n')
+
+		# set the Up vector 
+		firstFrameViewUp[:] = [0,1,0] # we initialize Up with vector Y
+		#sys.stderr.write('firstFrameViewUp '+str(firstFrameViewUp)+'\n')
+		verticalStartBranch = np.where( np.abs( (firstFrameViewUp*firstFrameViewFront).sum(-1)) > 0.99999 )
+		firstFrameViewUp[verticalStartBranch] = [1,0,0] # where it's vertical let's start with this vector
+		#sys.stderr.write('firstFrameViewUp '+str(firstFrameViewUp)+'\n')
+		# with a valid Up we can deduce the side then the up again to have a proper frame
+		firstFrameViewSide[:] = np.cross( firstFrameViewFront, firstFrameViewUp )
+		#sys.stderr.write('normSqrt '+str( ((firstFrameViewSide**2).sum(-1)).reshape(-1,1) )+'\n')
+		firstFrameViewSide /= np.sqrt( ((firstFrameViewSide**2).sum(-1)).reshape(-1,1) )
+
+		firstFrameViewUp[:] = np.cross( firstFrameViewSide, firstFrameViewFront )
+		#sys.stderr.write('frames[:,0,0,:-1] '+str(frames[:,0,:,:-1])+'\n')
+
+		sys.stderr.write('frames '+str(frames)+'\n')		
+	#------  now we can calculate the other frame on the path by iterating from the start point
+		for i in range(1,self.maxPoints):
+			frontPrevPoint = frames[:,i-1,0,:-1]
+			upPrevPoint = frames[:,i-1,1,:-1]
+
+			frontCurrentPoint = frames[:,i,0,:-1]
+			upCurrentPoint = frames[:,i,1,:-1]
+			sideCurrentPoint = frames[:,i,2,:-1]
+
+			sys.stderr.write('unitDir '+str(unitDir[:,i-1,:])+'\n')		
+			sys.stderr.write('upPrevPoint '+str(upPrevPoint)+'\n')		
+			upL = ((unitDir[:,i-1,:]*upPrevPoint).sum(-1)*-2.0).reshape(-1,1)*unitDir[:,i-1,:]
+			upL += upPrevPoint
+			sys.stderr.write('upL '+str(upL)+'\n')
+			frL = ((unitDir[:,i-1,:]*frontPrevPoint).sum(-1)*2.0).reshape(-1,1)*unitDir[:,i-1,:]
+			frL -= frontPrevPoint
+			v2 = frL # make v2 the same object as frL (frL is used only for v2)
+			v2 += frontCurrentPoint
+			c2 = (v2*v2).sum(-1).reshape(-1,1) # dot
+			sys.stderr.write('c2 '+str(c2)+'\n')
+
+			upCurrentPoint[...] = ((v2*upL).sum(-1)*-2.0).reshape(-1,1)*v2
+			upCurrentPoint /= c2
+			upCurrentPoint += upL
+
+			sideCurrentPoint[...] = np.cross(frontCurrentPoint,upCurrentPoint)
+
+		sys.stderr.write('frames[:,:,:-1,:-1] '+str(frames[:,:,:-1,:-1])+'\n')
+		sys.stderr.write('APArray[0,:,:,lightningBoltProcessor.kAPV_Radius,np.newaxis,np.newaxis] '+str(APArray[0,:,:,lightningBoltProcessor.kAPV_Radius,np.newaxis,np.newaxis])+'\n')
+		# multiply frames by corresponding Radius
+		frames[:,:,:-1,:-1] *= APArray[0,:,:,lightningBoltProcessor.kAPV_Radius,np.newaxis,np.newaxis]
+
+		# load points in frames
+		frames[:,:,3,:-1] = seedPathPointsView
+		sys.stderr.write('frames '+str(frames)+'\n')
+
+		childBatches = None
+		if doGenerateChilds:
+			numChilds = self.VInputsGeneral[0] + random.randint(0, self.VInputsGeneral[1])
+			epsilon = 0.000001
+			childsParams = np.random.uniform(epsilon, float(self.maxPoints-1) - epsilon, numChilds)
+			for c in childsParams:
+				sys.stderr.write('child param '+str(c)+'\n')
+				id0 = int(c)
+				id1 = id0 + 1
+				t = c - float(id0)
+				seedAPVal = (1-t)*APArray[0,0,id0] + t*APArray[0,0,id1]
+				sys.stderr.write('seedAPVal '+str(seedAPVal)+'\n')
+
+
+		return frames, childBatches
+
+	def process(self ):
+
 		maxGeneration = 3
 		currentGeneration = 0
 
+		firstGenerationAPV  = self.APVInputs1
+		normalAPV = self.APVInputs1
+
+		# circle template for tube extrusion
+		tubeSide = 4
+		pointsOfCircle = np.zeros( (4,tubeSide), np.float32 )
+		pointsOfCircle[1] = np.sin(np.linspace(1.5*np.pi,-np.pi*.5, tubeSide, endpoint=False))
+		pointsOfCircle[2] = np.cos(np.linspace(1.5*np.pi,-np.pi*.5, tubeSide, endpoint=False))
+		pointsOfCircle[3] = np.ones(tubeSide)
+		pointsOfCircle = pointsOfCircle.T
+
+
 		# --- create the first datas (seedPath, attributes ) to process 
-		startSeedPath = np.array( pointList, np.float32 )
-		startAPV = self.APVInputs1
-		startAPVMult = self.APVInputsMultiplier1
+		startSeedPath = np.array( self.startSeedPoints )
 
-		toDo = [ (startSeedPath, startAPV, startAPVMult )]
-		while len(toDo)>0:
-			outBranch, childList = self.generate(toDo.pop(0),currentGeneration<maxGeneration)
-			outBranchList.append(outBranch)
-			toDo.extend(childList)
-			return self.outSoftwareFunc(outBranchList)
+		sys.stderr.write('startSeedBranchesAttributes '+str(self.startSeedBranchesAttributes)+'\n')
+		startSeedAttributes = np.array( [self.startSeedBranchesAttributes] ) 
+		sys.stderr.write('startSeedAttributes '+str(startSeedAttributes)+'\n')
+		APV = firstGenerationAPV
 
-		return self.outSoftwareFunc(outBranchList)
+		# The result returned will be points in the form of a huge list of coordinate, there will be 2 lists to separate ring and non ring lightning
+		resultLoopingFrames = []
+		numLoopingLightningBranch = 0
+		resultFrames = []
+		numLightningBranch = 0
+		isLooping = False
+
+		batchSize = self.numStartSeedBranch
+		batch = batchSize, startSeedPath, startSeedAttributes
+		while batch is not None:
+			outFrames, childBatch = self.generate( batch, APV, isLooping, False) #currentGeneration<maxGeneration
+
+			if isLooping :
+				resultLoopingFrames.append(outFrames.reshape(-1,4,4))
+				numLoopingLightningBranch += batch[0]
+			else:
+				resultFrames.append(outFrames.reshape(-1,4,4))
+				numLightningBranch += batch[0]
+
+			batch = childBatch
+			# set the generation to the general values
+			APV = normalAPV
+			isLooping = False # only the first generation can loop
+
+		# here deduce points from frame and circles and add them to resultPoints
+		# transform a circle of point for each frame
+		circles = np.tile(pointsOfCircle,(len(resultFrames[0]),1,1)) # make enough circles
+
+		resultPoints = [ coord for array in resultFrames for k in range(len(array)) for point in (np.dot(circles[k],array[k])).tolist() for coord in point  ] 
+		resultLoopingPoints = [] # TODO
+		numLoopingLightningBranch = 0 # TODO
+
+		return self.outSoftwareFunc( resultLoopingPoints, numLoopingLightningBranch, resultPoints, numLightningBranch, self.maxPoints, tubeSide )
 
